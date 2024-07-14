2d80f09e7090c7b379f91fdd7f3beadc20af55a5
SaksamGyawali <saksamgyawali12@gmail.com>
2024-07-14 19:26:27 +0545

    //Minor chaange

--- graphs.cpp
+++ graphs.cpp
@@ -85,18 +85,3 @@ int Graph::degree(int vertex) const {
     }
 }
 
-std::vector<int> Graph::neighbours(int vertex) const {
-    std::vector<int> neighbors;
-    if (vertex >= vertexCount) return neighbors;
-    for (int i = 0; i < vertexCount; i++) {
-        if (adjMatrix[vertex][i]) {
-            neighbors.push_back(i);
-        }
-    }
-    return neighbors;
-}
-
-bool Graph::neighbour(int vertex1, int vertex2) const {
-    if (vertex1 >= vertexCount || vertex2 >= vertexCount) return false;
-    return adjMatrix[vertex1][vertex2] == 1;
-}
